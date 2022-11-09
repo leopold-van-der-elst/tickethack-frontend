169856efe5c6db3b7dd81291f89b048e5533fc43
Leopold van der Elst <leopoldvde@hotmail.com>
2022-11-09 14:13:30 +0100

    Realtion between Script and back, getting information. PART1

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-document.querySelector("#searchBtn").addEventListener("click",()=>{
+document.querySelector("#searchBtn").addEventListener("click",function(){
     const departure = document.querySelector("#departureInput").value
     const arrival = document.querySelector("#arrivalInput").value
     const date = document.querySelector("#dateInput").value
     fetch(`http://localhost:3000/trips`,{
-     method : 'POST',
-     headers : {
+     method: 'POST',
+     headers: {
          "Content-Type":"application/json",
      },
-     body: JSON.stringify({departure: departure, arrival: arrival, date: date})
-     
-    } )
-   
- } )
+     body: JSON.stringify({departure, arrival, date})
+    }).then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < data.result.length; i++) {
+            const hoursMinute = new Date(data.result[i].date)
+            const hour = hoursMinute.getHours()
+            const minutes = hoursMinute.getMinutes()
+            document.querySelector("#container-right").innerHTML += `
+            <div id="element-row">
+                <p>${data.result[i].departure}</p>
+                <p>${data.result[i].arrival}</p>
+                <p>${hour} : ${minutes}</p>
+                <p>${data.result[i].price}</p>  
+                <button id='btn-booking'>BOOKING</button>
+            </div>
+            `;
+        }
+    })
+ })
  
